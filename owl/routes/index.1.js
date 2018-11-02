61a0162bad364e79eede04e2014f1797a21b980f
irae483 <43145976+irae483@users.noreply.github.com>
2018-11-01 18:06:30 -0600

    Update index.1.js

--- owl/routes/index.1.js
+++ owl/routes/index.1.js
@@ -4,30 +4,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('weather.html', { root:  'public' });
+  res.sendFile('owl.html', { root:  'public' });
 });
 
-router.get('/getcity',function(req,res,next) {
-    console.log("In getcity route");
+router.get('/getdeath',function(req,res,next) {
+    console.log("In death route");
 
 fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
   if(err) throw err;
-  var cities = data.toString().split("\n");
-  for(var i = 0; i < cities.length; i++) {
-    var myRe = new RegExp("^" + req.query.q);
-console.log(myRe);
+  var deaths = data.toString().split("\n");
 var jsonresult = [];
-for(var i = 0; i < cities.length; i++) {
-  var result = cities[i].search(myRe); 
+
+  var result = deaths[1] 
   if(result != -1) {
-    console.log(cities[i]);
-    jsonresult.push({city:cities[i]});
+    console.log(result);
+    jsonresult.push({city:result});
   } 
-}   
+  
 console.log(jsonresult);
 res.status(200).json(jsonresult);
 
-  }
 }); 
 
 
@@ -36,34 +32,3 @@ res.status(200).json(jsonresult);
 
 
 module.exports = router;
-
-
-
-
-
-/*
-fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
-  if(err) throw err;
-  var cities = data.toString().split("\n");
-  for(var i = 0; i < cities.length; i++) {
-    var myRe = new RegExp("^" + req.query.q);
-console.log(myRe);
-var jsonresult = [];
-for(var i = 0; i < cities.length; i++) {
-  var result = cities[i].search(myRe); 
-  if(result != -1) {
-    console.log(cities[i]);
-    jsonresult.push({city:cities[i]});
-  } 
-}   
-console.log(jsonresult);
-res.status(200).json(jsonresult);
-
-  }
-  
-  
-  
-});    
-    
-
-*/
